e7b099ee25f19a73b8b7cd73b4c192a8d9dbeec6
Tawfik chelly <tawfikchelly@gmail.com>
2019-04-04 23:37:38 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,57 @@
+/**
+ * Copyright 2017 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const {WebhookClient} = require('dialogflow-fulfillment');
+
+process.env.DEBUG = 'dialogflow:*'; // enables lib debugging statements
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+
+ 
+
+  function readFromDb (agent) {
+    // Get the database collection 'dialogflow' and document 'agent'
+    //I think i am doing it worong here should I left agent 
+    const dialogflowAgentDoc = db.collection('courses').doc('BCOR100');
+
+    // Get the value of 'entry' in the document and send it to the user
+    return dialogflowAgentDoc.get()
+      .then(doc => {
+        if (!doc.exists) {
+          agent.add('No data found in the database!');
+        } else {
+        // code is the attributte i wanna read !?
+          agent.add(doc.data().code);
+        }
+        return Promise.resolve('Read complete');
+      }).catch(() => {
+        agent.add('Error reading entry from the Firestore database.');
+        agent.add('Please add a entry to the database first by saying, "Write <your phrase> to the database"');
+      });
+  }
+
+  // Map from Dialogflow intent names to functions to be run when the intent is matched
+  let intentMap = new Map();
+  intentMap.set('course.info', readFromDb);
+  agent.handleRequest(intentMap);
+});
